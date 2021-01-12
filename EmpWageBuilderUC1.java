01ebe434bcd1c1f56b6f830ddc8a4f6cdd73b67d
himanshunete <68182639+himanshunete@users.noreply.github.com>
2021-01-12 09:34:41 +0530

    MODIFIED: functionality to check employee's attendance

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,10 +1,13 @@
 public class EmpWageBuilderUC1{
-   public static void main(String[] args){
-      int IS_FULL_TIME = 1;
+   public static int IS_FULL_TIME = 1;
+   public static void CheckingAttendance(){   
       double empCheck = Math.floor(Math.random()*10)%2;
       if (empCheck == IS_FULL_TIME)
          System.out.println("Employee is Present");
       else
          System.out.println("Employee is Absent");
    }
+   public static void main(String[] args){
+     CheckingAttendance(); 
+   }      
 }
