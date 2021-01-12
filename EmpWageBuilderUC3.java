b4d2728187692d7b5a6063e3f5d7b86d0a150f54
himanshunete <68182639+himanshunete@users.noreply.github.com>
2021-01-12 09:54:43 +0530

    MODIFIED: functionality to check employee's Part Time Wage

--- EmpWageBuilderUC3.java
+++ EmpWageBuilderUC3.java
@@ -1,16 +1,25 @@
 public class EmpWageBuilderUC3{
-   public static void main(String[] args){
-      int IS_PART_TIME = 1;
-      int IS_FULL_TIME = 2;
-      int EMP_RATE_PER_HOUR = 20;
-      int empHrs =0;
-      int empWage = 0;
-      double empCheck = Math.floor(Math.random() * 10) % 3;
-      if (empCheck == IS_FULL_TIME)
-         empHrs = 8;
-      else if (empCheck == IS_PART_TIME)
-         empHrs = 4;
-      empWage = empHrs * EMP_RATE_PER_HOUR;
-      System.out.println("Emp Daily Wage: " + empWage);
-   } 
+
+      //Constants  
+      public static final int IS_PART_TIME = 1;
+      public static final int IS_FULL_TIME = 2;
+      public static final int EMP_RATE_PER_HOUR = 20;
+
+      //Static Variables
+      public static int empHrs ;
+      public static int empWage ;
+
+      public static void CheckingWage(){
+         double empCheck = Math.floor(Math.random() * 10) % 3;
+         if (empCheck == IS_FULL_TIME)
+            empHrs = 8;
+         else if (empCheck == IS_PART_TIME)
+            empHrs = 4;
+         empWage = empHrs * EMP_RATE_PER_HOUR;
+         System.out.println("Emp Daily Wage: " + empWage);
+      } 
+      public static void main(String[] args){
+         CheckingWage();
+      }         
+
 }
