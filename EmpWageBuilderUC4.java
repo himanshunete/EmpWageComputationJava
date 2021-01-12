8e43c2532c187f58ee1726e532acd2ea814a8322
himanshunete <68182639+himanshunete@users.noreply.github.com>
2021-01-12 10:03:54 +0530

    MODIFIED: functionality to check Employee's Wage using Switch Case

--- EmpWageBuilderUC4.java
+++ EmpWageBuilderUC4.java
@@ -1,10 +1,14 @@
 public class EmpWageBuilderUC4{
+   //Constants
    public static final int IS_PART_TIME = 1;
    public static final int IS_FULL_TIME = 2;
    public static final int EMP_RATE_PER_HOUR = 20;
-   public static void main(String[] args){
-      int empHrs =0;
-      int empWage = 0;
+  
+
+   //Static Variables
+   public static int empHrs;
+   public static int empWage ;
+   public static void CheckingWage(){
       int empCheck = (int)Math.floor(Math.random() * 10) % 3;
       switch (empCheck) {
          case IS_FULL_TIME :         
@@ -15,8 +19,11 @@ public class EmpWageBuilderUC4{
             break;
          default :
             empHrs = 0;
-      }
+      } 
       empWage = empHrs * EMP_RATE_PER_HOUR;
       System.out.println("Emp Daily Wage: " + empWage);
-  } 
+   } 
+   public static void main(String[] args){
+      CheckingWage();
+   }
 }
