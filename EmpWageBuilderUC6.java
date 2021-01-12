3b98d1b8edf55eaa6b353253f742fe7f4179af64
himanshunete <68182639+himanshunete@users.noreply.github.com>
2021-01-12 10:27:52 +0530

    MODIFIED: functionality to calculate Wage till the condition is reached using While Loop

--- EmpWageBuilderUC6.java
+++ EmpWageBuilderUC6.java
@@ -1,17 +1,19 @@
 public class EmpWageBuilderUC6{
+   //Constants
    public static final int IS_PART_TIME = 1;
    public static final int IS_FULL_TIME = 2;
    public static final int EMP_RATE_PER_HOUR = 20;
    public static final int NUM_OF_WORKING_DAYS = 20;
    public static final int MAX_NUM_OF_HOURS = 100;
-   public static void main(String[] args){   
-      int totalEmpWage = 0;
-      int totalEmpHrs = 0;
-      int totalWorkingDays = 0;
+   //Static Variables  
+   public static int totalEmpWage ;
+   public static int totalEmpHrs ;
+   public static int totalWorkingDays ;
+   public static int empHrs;
+   public static int empWage;
+   public static void CheckingWage(){
       while ( totalEmpHrs <=  MAX_NUM_OF_HOURS && totalWorkingDays <=  NUM_OF_WORKING_DAYS ){
          int empCheck = (int)Math.floor(Math.random() * 10) % 3;
-         int empHrs = 0;
-         int empWage = 0;
          totalWorkingDays ++;
          totalEmpHrs ++;
          switch (empCheck) {
@@ -29,5 +31,8 @@ public class EmpWageBuilderUC6{
          System.out.println(" Emp Wage: " + empWage);
       }
       System.out.println("Total Emp Wage: " + totalEmpWage);
-  }
+   }
+   public static void main(String[] args){
+      CheckingWage();
+   }
 }
