326a6066abe94711af024a08dc3a1e0f708d5788
himanshunete <himanshuneteh@gmail.com>
2021-01-13 08:26:14 +0530

    ADDED: code using ArrayList and HashMap

---
+++ EmpWageBuilderUC12.java
@@ -0,0 +1,116 @@
+import java.io.*; 
+import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+
+interface ICompanyEmpWage{
+   public void addCompanyWage( String company, int  empRatePerHour , int numOfWorkingDays, int  maxNumOfHours );
+   public void computeEmpWage();
+   
+
+}
+
+
+
+class CompanyEmpWage{
+   public final String company;
+   public final int empRatePerHour ;
+   public final int numOfWorkingDays;
+   public final int maxNumOfHours ;
+   public int totalEmpWage ;
+
+   public CompanyEmpWage( String company, int  empRatePerHour , int numOfWorkingDays, int  maxNumOfHours ){
+      this.company = company ;
+      this.empRatePerHour = empRatePerHour ;
+      this.numOfWorkingDays = numOfWorkingDays ;
+      this.maxNumOfHours = maxNumOfHours ;
+   }
+
+   public void setTotalEmpWage(int totalEmpWage){
+      this.totalEmpWage = totalEmpWage ;
+   }
+
+   public String toString(){
+      return "Total Emp Wage for Company: " +company+ " is: " + totalEmpWage;
+   }
+}
+
+
+public class EmpWageBuilderUC12 implements ICompanyEmpWage{
+   public static final int IS_PART_TIME = 1;
+   public static final int IS_FULL_TIME = 2;
+
+   private int numOfCompany = 0;
+   private  ArrayList<CompanyEmpWage> companyEmpWageList;  
+   private  Map<String,CompanyEmpWage> companyToEmpWageMap; 
+
+
+
+   public EmpWageBuilderUC12(){
+      companyEmpWageList = new  ArrayList<>() ; 
+      companyToEmpWageMap =  new HashMap<>() ;
+   } 
+
+
+   public void addCompanyWage( String company, int  empRatePerHour , int numOfWorkingDays, int  maxNumOfHours ){
+      CompanyEmpWage companyEmpWage = new CompanyEmpWage( company, empRatePerHour , numOfWorkingDays, maxNumOfHours ) ;
+      companyEmpWageList.add(companyEmpWage );
+      companyToEmpWageMap.put(company, companyEmpWage );
+   }
+
+
+   public void computeEmpWage(){
+      for ( int i=0; i < companyEmpWageList.size(); i++ ){
+         CompanyEmpWage companyEmpWage = companyEmpWageList.get(i) ;
+         companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
+         System.out.println(companyEmpWage);
+      }
+   }
+
+
+   public int computeEmpWage( CompanyEmpWage companyEmpWage ){
+      System.out.println(" Welcome to Employee Wage Computation Program ") ;
+
+      //Variables
+      int totalEmpHrs = 0;
+      int totalWorkingDays = 0 ;
+      int empHrs = 0 ;
+
+      //Computation
+      while ( totalEmpHrs <=  companyEmpWage.maxNumOfHours && totalWorkingDays <= companyEmpWage.numOfWorkingDays ){
+         int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+         totalWorkingDays ++;
+         totalEmpHrs ++;
+         switch (empCheck) {
+            case IS_FULL_TIME :
+               empHrs = 8 ;
+               break;
+            case IS_PART_TIME :
+               empHrs = 4  ;
+               break;
+            default :
+               empHrs = 0;
+         }
+         totalEmpHrs += empHrs;
+         System.out.println(" Day#: "+ totalWorkingDays + " Emp Hr: " +empHrs);
+      }
+      return totalEmpHrs * companyEmpWage.empRatePerHour;
+
+   }
+
+   public int getTotalWage(String company){
+      return  companyToEmpWageMap.get(company).totalEmpWage;
+   }
+
+   public static void main(String[] args){
+      EmpWageBuilderUC12 empWageBuilder = new EmpWageBuilderUC12() ;
+      empWageBuilder.addCompanyWage("Audi", 20, 2, 10);
+      empWageBuilder.addCompanyWage("BMW", 10, 4, 20);
+      empWageBuilder.addCompanyWage("Ferrari", 20, 6, 30);
+      empWageBuilder.computeEmpWage();
+      System.out.println("Total Wage for Audi:" + empWageBuilder.getTotalWage("Audi"));
+
+
+
+   }
+}
